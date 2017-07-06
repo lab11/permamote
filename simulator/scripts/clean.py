a4d86056625f00a664be16cd93b95b749341feb3
Neal Jackson <nealjack@umich.edu>
2017-07-06 10:04:44 -0700

    add saving of cleaned light data

--- simulator/scripts/clean.py
+++ simulator/scripts/clean.py
@@ -11,6 +11,7 @@ sleep_current = 5E-6
 solar_panel_eff = .19 #%
 solar_panel_area = 14 #cm^2
 
+mins_in_day = 60*24
 
 def lux2irradiance(lux):
     # irradiance estimation from lux using values reported on page 202 in:
@@ -20,6 +21,7 @@ def lux2irradiance(lux):
     lux[lux > 200] = lux[lux > 200]*74.9/320
 
 data_dir = 'raw_data/'
+out_dir = 'clean_data/'
 
 light_files = (data_dir + x for x in os.listdir(data_dir) if 'light' in x)
 motion_files= (data_dir + x for x in os.listdir(data_dir) if 'motion' in x)
@@ -28,28 +30,49 @@ fname_to_days = {}
 fname_to_data = {}
 for fname in light_files:
     print(fname)
-    lights = np.loadtxt(fname, delimiter=',', usecols=1)
-    light_times = np.loadtxt(fname, dtype = 'datetime64', delimiter=',', usecols=0, converters = {0:np.datetime64})
-    days = np.arange(light_times[0], light_times[-1], dtype='datetime64[D]')
-    light_days = light_times.astype('datetime64[D]')
-    valid_days = []
-    for day in days:
-        ind = light_days == day
-        if np.sum(lights[ind] == 0)/lights[ind].shape[0] < 0.6:
-            valid_days.append(day)
-    valid_days = np.asarray(valid_days)
-    fname_to_days[fname] = valid_days
-    fname_to_data[fname] = lights[np.in1d(light_days, valid_days)]
-    print(fname_to_data[fname].shape)
-    print(lights.shape)
-    print(fname_to_data[fname])
-    #power_uW = lights[1440*3:1440*4]*solar_panel_eff*solar_panel_area
-    #print(np.mean(power_uW/3.3))
+    data = np.loadtxt(fname, delimiter=',', usecols=1)
+    times = np.loadtxt(fname, dtype = 'datetime64', delimiter=',', usecols=0, converters = {0:np.datetime64})
+    times = (times + np.timedelta64(30, 's')).astype('datetime64[m]')
 
+    # split data into multiple spans if data represents multiple different
+    # periods seperated by at least a day of no data
+    ind = []
+    previous_time = times[0]
+    for i, time in enumerate(times):
+        if time - previous_time >= np.timedelta64(30, 'm'):
+            ind.append(i)
+        previous_time = time
+    spans = np.split(times, ind)
+    datas = np.split(data, ind)
+
+    for i, (light_data, time_span) in enumerate(zip(datas, spans)):
+        # Fill in or average duplicates in uncleaned data
+        # if multiple data points represent the same minute, average them
+        # if a minute's data point is missing, use the previous minutes value
+        # if we aren't looking at a day or more of data, skip it
+        if time_span[-1] - time_span[0] < np.timedelta64(1, 'D'): continue
+        minutes = np.arange(time_span[0], time_span[-1], dtype='datetime64[m]')
+        cleaned = np.ndarray(minutes.shape)
+        for i, minute in enumerate(minutes):
+            match = light_data[time_span == minute]
+            if match.shape[0] > 1:
+                cleaned[i] = np.mean(match)
+            elif match.shape[0] == 1:
+                cleaned[i] = match[0]
+            else:
+                cleaned[i] = cleaned[i-1]
+        print(time_span)
+        print(cleaned)
+        # create output directory if necessary
+        out_dir = os.path.dirname(out_dir)
+        if out_dir:
+            os.makedirs(out_dir, exist_ok=True)
+        np.save(fname.split('.')[0] + '_times_' + str(i), time_span)
+        np.save(fname.split('.')[0] + '_clean_' + str(i), cleaned)
 
 for fname in motion_files:
     print(fname)
     motions = np.loadtxt(fname, dtype = 'bool', delimiter=',', usecols=1, converters = {1:bool})
     times = np.loadtxt(fname, dtype = 'datetime64', delimiter=',', usecols=0, converters = {0:np.datetime64})
-    days = np.arange(times[0], times[-1], dtype='datetime64[D]')
+    times = (times + np.timedelta64(30, 's')).astype('datetime64[m]')
 
