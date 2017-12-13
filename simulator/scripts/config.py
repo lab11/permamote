a68091c9c2a231ee10e6e38cca88e36faaab3d21
Neal Jackson <nealjack@umich.edu>
2017-12-13 00:26:18 -0800

    Use density/volume for primary cell energy

--- simulator/scripts/config.py
+++ simulator/scripts/config.py
@@ -1,13 +1,32 @@
+import math
+
 
 class sim_config:
     def __init__(self):
+        self.battery_type_to_volume_L = {
+            #https://en.wikipedia.org/wiki/List_of_battery_sizes
+            'AA':   ((14.5 * 1/2)**2 * math.pi * 50.5) * 1E-6,
+            'AAA':  ((10.5 * 1/2)**2 * math.pi * 44.5) * 1E-6,
+            'CR123A': ((17 * 1/2)**2 * math.pi * 34.5) * 1E-6,
+            'CR2032':  ((20 * 1/2)**2 * math.pi * 3.2) * 1E-6,
+            'HTC-titanate': ((10 * 1/2)**2 * math.pi * 20) * 1E-6,
+        }
+        self.chemistry_energy_density_WhpL = {
+            # https://en.wikipedia.org/wiki/Comparison_of_commercial_battery_types
+            'Alkaline': 320,
+            'LiMnO2':   510,
+            # https://en.wikipedia.org/wiki/Lithium%E2%80%93titanate_battery
+            'titanate': 177,
+            'LiPo':     500,
+            'LiIon':    475,
+        }
         self.design_config = {
             'name' : 'design',
             'active_frequency_minutes' : 1,
             'operating_voltage_V' : 3.3,
             'sleep_current_A' : 3E-6,
             'active_current_A' : 4.9E-3,
-            'active_period_s': 0.07,
+            'active_period_s': 0.06,
             'boost_efficiency' : 0.8,
             'frontend_efficiency' : 0.8,
             'secondary' : 'lto_battery',
@@ -17,11 +36,12 @@ class sim_config:
 
         self.primary_config= {
             'name' : 'primary',
-            'capacity_mAh' : 200,
-            'nominal_voltage_V' : 3,
+            #'capacity_mAh' : 200,
+            #'nominal_voltage_V' : 3,
+            'density_WhpL': self.chemistry_energy_density_WhpL['LiMnO2'],
+            'volume_L':   self.battery_type_to_volume_L['CR123A'],
             'leakage_percent_year' : 1,
         }
-
         self.secondary_lipo_config = {
             'name' : 'secondary',
             'charge_discharge_eff' : 0.95,
@@ -55,7 +75,7 @@ class sim_config:
         self.config_list = [self.design_config, self.primary_config, self.secondary_configs[self.design_config['secondary']], self.solar_config]
 
 sweep_vars = [
-                [('secondary', 'capacity_mAh'), [i*10**exp for exp in range(-5, 2) for i in range(1, 10, 2)]],
-                #[('primary', 'capacity_mAh') , 1500, 2500, 100],
-                #[('secondary', 'capacity_mAh') , .1, 2, .1]
+                #[('solar', 'area_cm2'), [i*10**exp for exp in range(0, 2) for i in range(1,10)], 'both']
+                #[('primary', 'volume_L'), [i*10**(-3) for i in range(1, 15)], 'life'],
+                [('secondary', 'capacity_mAh'), [i*10**exp for exp in range(-5, 2) for i in range(1, 10, 2)], 'both'],
              ]
