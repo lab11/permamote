f98712f864a0535c589b97c03e81fddeac741d4e
Neal Jackson <nealjack@umich.edu>
2018-03-22 15:47:33 -0700

    port isl driver to nrf52, use twi manager

--- software/apps/isl29125_test/main.c
+++ software/apps/isl29125_test/main.c
@@ -1,65 +1,57 @@
-/*
- * Send an advertisement periodically
- */
-
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include "nrf_gpio.h"
 #include "nrf_delay.h"
-#include "nrf_drv_twi.h"
+#include "nrf_twi_mngr.h"
 #include "nrf_drv_gpiote.h"
 #include "app_util_platform.h"
-#include "ble_advdata.h"
 #include "nordic_common.h"
-#include "softdevice_handler.h"
-#include "ble_debug_assert_handler.h"
+#include "nrf_sdh.h"
+#include "nrf_soc.h"
+#include "app_timer.h"
 #include "led.h"
 #include "app_uart.h"
+#include "nrf_drv_clock.h"
 
-#include "simple_ble.h"
-#include "simple_adv.h"
 #include "permamote.h"
 #include "isl29125.h"
 
-#define MAX_TEST_DATA_BYTES  (15U)
 #define UART_TX_BUF_SIZE     256
 #define UART_RX_BUF_SIZE     256
 
-char name[10] = "Permamote";
+#define LED0 NRF_GPIO_PIN_MAP(0,4)
+#define LED1 NRF_GPIO_PIN_MAP(0,5)
+#define LED2 NRF_GPIO_PIN_MAP(0,6)
 
-// Intervals for advertising and connections
-static simple_ble_config_t ble_config = {
-  .platform_id       = 0x00,              // used as 4th octect in device BLE address
-  .device_id         = DEVICE_ID_DEFAULT,
-  .adv_name          = name,
-  .adv_interval      = MSEC_TO_UNITS(500, UNIT_0_625_MS),
-  .min_conn_interval = MSEC_TO_UNITS(500, UNIT_1_25_MS),
-  .max_conn_interval = MSEC_TO_UNITS(1000, UNIT_1_25_MS)
-};
+#define SENSOR_RATE APP_TIMER_TICKS(1000)
+APP_TIMER_DEF(sensor_read_timer);
 
-nrf_drv_twi_t twi_instance = NRF_DRV_TWI_INSTANCE(1);
-
-void twi_init(void) {
-  ret_code_t err_code;
-
-  const nrf_drv_twi_config_t twi_config = {
-    .scl                = I2C_SCL,
-    .sda                = I2C_SDA,
-    .frequency          = NRF_TWI_FREQ_100K,
-    .interrupt_priority = APP_IRQ_PRIORITY_HIGH
-  };
-
-  err_code = nrf_drv_twi_init(&twi_instance, &twi_config, NULL, NULL);
-  APP_ERROR_CHECK(err_code);
-}
+NRF_TWI_MNGR_DEF(twi_mngr_instance, 5, 0);
 
 void uart_error_handle (app_uart_evt_t * p_event) {
-    if (p_event->evt_type == APP_UART_COMMUNICATION_ERROR) {
-        APP_ERROR_HANDLER(p_event->data.error_communication);
-    } else if (p_event->evt_type == APP_UART_FIFO_ERROR) {
-        APP_ERROR_HANDLER(p_event->data.error_code);
-    }
+  if (p_event->evt_type == APP_UART_COMMUNICATION_ERROR) {
+    APP_ERROR_HANDLER(p_event->data.error_communication);
+  } else if (p_event->evt_type == APP_UART_FIFO_ERROR) {
+    APP_ERROR_HANDLER(p_event->data.error_code);
+  }
+}
+static void sensor_read_callback(float red, float green, float blue) {
+  printf("red: %.02f, green: %.02f, blue %.02f\n", red, green, blue);
+}
+
+static void read_timer_callback (void* p_context) {
+  isl29125_schedule_color_read();
+}
+
+static void timer_init(void)
+{
+  uint32_t err_code = app_timer_init();
+  APP_ERROR_CHECK(err_code);
+  err_code = app_timer_create(&sensor_read_timer, APP_TIMER_MODE_REPEATED, read_timer_callback);
+  APP_ERROR_CHECK(err_code);
+  err_code = app_timer_start(sensor_read_timer, SENSOR_RATE, NULL);
+  APP_ERROR_CHECK(err_code);
 }
 
 void uart_init(void) {
@@ -67,8 +59,8 @@ void uart_init(void) {
 
   const app_uart_comm_params_t comm_params =
   {
-    11,
-    12,
+    UART_RX,
+    UART_TX,
     0,
     0,
     APP_UART_FLOW_CONTROL_DISABLED,
@@ -76,55 +68,66 @@ void uart_init(void) {
     UART_BAUDRATE_BAUDRATE_Baud115200
   };
   APP_UART_FIFO_INIT(&comm_params,
-                     UART_RX_BUF_SIZE,
-                     UART_TX_BUF_SIZE,
-                     uart_error_handle,
-                     APP_IRQ_PRIORITY_LOW,
-                     err_code);
+      UART_RX_BUF_SIZE,
+      UART_TX_BUF_SIZE,
+      uart_error_handle,
+      APP_IRQ_PRIORITY_LOW,
+      err_code);
   APP_ERROR_CHECK(err_code);
 
 }
 
+void twi_init(void) {
+  ret_code_t err_code;
+
+  const nrf_drv_twi_config_t twi_config = {
+    .scl                = I2C_SCL,
+    .sda                = I2C_SDA,
+    .frequency          = NRF_TWI_FREQ_400K,
+  };
+
+  err_code = nrf_twi_mngr_init(&twi_mngr_instance, &twi_config);
+  APP_ERROR_CHECK(err_code);
+}
+
 int main(void) {
-  // Setup BLE
-  simple_ble_init(&ble_config);
+  // init softdevice
+  nrf_sdh_enable_request();
+  sd_power_dcdc_mode_set(NRF_POWER_DCDC_ENABLE);
+
+  // init led
+  led_init(LED2);
+  led_off(LED2);
 
   // init uart
   uart_init();
-  printf("\nLIGHT COLOR TEST\n");
+
+  printf("\nCOLOR TEST\n");
 
   // Init twi
   twi_init();
+
+  timer_init();
 
   // Turn on power gate
   nrf_gpio_cfg_output(MAX44009_EN);
   nrf_gpio_cfg_output(ISL29125_EN);
   nrf_gpio_cfg_output(MS5637_EN);
   nrf_gpio_cfg_output(SI7021_EN);
-  nrf_gpio_cfg_output(PIR_EN);
-  nrf_gpio_pin_clear(MAX44009_EN);
+  nrf_gpio_pin_set(MAX44009_EN);
   nrf_gpio_pin_clear(ISL29125_EN);
   nrf_gpio_pin_set(MS5637_EN);
   nrf_gpio_pin_set(SI7021_EN);
-  nrf_gpio_pin_set(PIR_EN);
 
-  const isl29125_config_t config = {
+  isl29125_config_t config = {
     .mode = isl29125_green_red_blue,
-    .range=0,
-    .resolution=0,
-    .sync_int=0
+    .resolution = 1,
   };
 
-  isl29125_init(&twi_instance);
+  isl29125_init(&twi_mngr_instance, sensor_read_callback);
   isl29125_config(config);
 
-  // Advertise because why not
-  simple_adv_only_name();
-
   while (1) {
-    float red, green, blue = 0;
-    isl29125_read_lux(&red, &green, &blue);
-    printf("RGB: %.02f %.02f %.02f\n", red, green, blue);
-    nrf_delay_ms(5000);
+    sd_app_evt_wait();
   }
 }
