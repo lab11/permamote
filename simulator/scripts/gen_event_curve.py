6bd397f22337a80b0ec30a4084f22e5e0e200d0d
Neal Jackson <nealjack@umich.edu>
2018-03-29 18:03:59 -0700

    Choose 3rd floor data as representative curve

--- simulator/scripts/gen_event_curve.py
+++ simulator/scripts/gen_event_curve.py
@@ -2,13 +2,14 @@
 
 import os
 import numpy as np
+import scipy.stats as stats
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from multiprocessing import Pool
 from datetime import datetime
 import arrow
 
-data_dir = 'raw_data/'
+data_dir = 'clean_data/'
 out_dir = 'curves/'
 out_dir = os.path.dirname(out_dir) + '/'
 
@@ -20,24 +21,28 @@ def decode_to_bool(bytes_to_decode):
     else: return False
 
 def plot_data(title, times, data):
-    monthsloc = mdates.MonthLocator()
-    daysloc= mdates.DayLocator()
-    monthsfmt = mdates.DateFormatter('%B')
-    daysfmt   = mdates.DateFormatter('%d')
+    #monthsloc = mdates.MonthLocator()
+    #daysloc= mdates.DayLocator()
+    #monthsfmt = mdates.DateFormatter('%B')
+    #daysfmt   = mdates.DateFormatter('%d')
 
     fig, ax = plt.subplots()
     ax.set_title(title)
-    ax.xaxis.set_major_locator(monthsloc)
-    ax.xaxis.set_major_formatter(monthsfmt)
-    ax.xaxis.set_minor_locator(daysloc)
-    ax.xaxis.set_minor_formatter(daysfmt)
+    #ax.xaxis.set_major_locator(monthsloc)
+    #ax.xaxis.set_major_formatter(monthsfmt)
+    #ax.xaxis.set_minor_locator(daysloc)
+    #ax.xaxis.set_minor_formatter(daysfmt)
     ax.plot(times, data)
 
 def plot_full_data(fname, utc_diff):
     print(fname)
-    data =  np.loadtxt(data_dir + fname, dtype = 'bool', delimiter=',', usecols=1, converters = {1:decode_to_bool})
-    times = np.loadtxt(data_dir + fname, dtype = 'datetime64', delimiter=',', usecols=0, converters = {0:np.datetime64})
+    #data =  np.loadtxt(data_dir + fname, dtype = 'bool', delimiter=',', usecols=1, converters = {1:decode_to_bool})
+    #times = np.loadtxt(data_dir + fname, dtype = 'datetime64', delimiter=',', usecols=0, converters = {0:np.datetime64})
+    array_load = np.load(data_dir + fname)
+    times = array_load[:,0].astype('datetime64[s]').astype('datetime64[m]')
     times = times + np.timedelta64(utc_diff, 'h')
+    data = array_load[:,1]
+
     # convert to hour granularity
     times = times.astype('datetime64[h]').astype(datetime)
     unique_times = np.unique(times)
@@ -50,12 +55,16 @@ def plot_full_data(fname, utc_diff):
 
 def generate_average_event_curve(fname, utc_diff, date_range):
     print(fname)
-    data =  np.loadtxt(data_dir + fname, dtype = 'bool', delimiter=',', usecols=1, converters = {1:decode_to_bool})
-    times = np.loadtxt(data_dir + fname, dtype = 'datetime64', delimiter=',', usecols=0, converters = {0:np.datetime64})
+    #data =  np.loadtxt(data_dir + fname, dtype = 'bool', delimiter=',', usecols=1, converters = {1:decode_to_bool})
+    #times = np.loadtxt(data_dir + fname, dtype = 'datetime64', delimiter=',', usecols=0, converters = {0:np.datetime64})
+    #times = times + np.timedelta64(utc_diff, 'h')
+    array_load = np.load(data_dir + fname)
+    times = array_load[:,0].astype('datetime64[s]').astype('datetime64[m]')
     times = times + np.timedelta64(utc_diff, 'h')
+    data = array_load[:,1]
+
     # convert to hour granularity
     times = times.astype('datetime64[h]').astype(datetime)
-
     # limit to range we're interested in
     selection = np.logical_and(times >= date_range[0], times < date_range[1])
     times = times[selection]
@@ -74,14 +83,11 @@ def generate_average_event_curve(fname, utc_diff, date_range):
         avg_curve.append(bins)
     avg_curve = np.asarray(avg_curve)
     avg_curve = np.average(avg_curve, axis = 0)
-    plot_data(fname, np.arange(bins.size), avg_curve)
+    return avg_curve
 
 
-
-motion_files= sorted([x for x in os.listdir(data_dir) if 'motion' in x])
-print(motion_files, -8)
-#plot_full_data(motion_files[2])
-generate_average_event_curve(motion_files[0], -5, (datetime(2016, 7, 30), datetime(2016, 8, 6)))
-generate_average_event_curve(motion_files[1], -5, (datetime(2016, 7, 30), datetime(2016, 8, 6)))
-generate_average_event_curve(motion_files[2], -8, (datetime(2017, 2, 18), datetime(2017, 2, 25)))
+fname = 'motion-Blink-c098e5900064-3rd_Floor_2016-07-27_clean.npy'
+curve = generate_average_event_curve(fname, -4, (datetime(2016, 7, 30), datetime(2016, 8, 6)))
+plot_data(fname, np.arange(curve.size), curve/np.max(curve))
+np.save(out_dir+fname.split('-')[2] + '_reactive_curve', curve/np.max(curve))
 plt.show()
