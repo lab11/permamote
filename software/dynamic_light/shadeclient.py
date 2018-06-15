12577378e405f81fe307b597e7373eb15d7d72ff
Neal Jackson <nealjack@umich.edu>
2018-06-15 15:10:26 -0700

    add more error handling to shade client

--- software/dynamic_light/shadeclient.py
+++ software/dynamic_light/shadeclient.py
@@ -1,4 +1,5 @@
 import requests
+import http
 from shade_config import shade_config
 
 class ShadeClient:
@@ -6,21 +7,42 @@ class ShadeClient:
         self.host = config['address'] + '/shades'
         self.user = config['user']
         self.password = config['password']
-        self.battery = 0
-        self.position = 0
     def get_battery(self, shade_mac):
-        r = requests.request('GET', self.host, json = { 'shade': shade_mac, 'action': 'battery'}, auth = (self.user, self.password))
-        self.battery = r.json()['battery']
-        return self.battery
+        while(1):
+            try:
+                r = requests.request('GET', self.host, json = { 'shade': shade_mac, 'action': 'battery'}, auth = (self.user, self.password))
+                break;
+            except requests.exceptions.ConnectionError:
+                pass
+        battery = -1
+        if 'battery' in r.json():
+            battery = r.json()['battery']
+        else:
+            print(r.json())
+        return battery
     def get_position(self, shade_mac):
-        r = requests.request('GET', self.host, json = { 'shade': shade_mac, 'action': 'position'}, auth = (self.user, self.password))
-        self.position = r.json()['position']
-        return self.position
+        while(1):
+            try:
+                r = requests.request('GET', self.host, json = { 'shade': shade_mac, 'action': 'position'}, auth = (self.user, self.password))
+                break;
+            except requests.exceptions.ConnectionError:
+                pass
+        position = -1
+        if 'position' in r.json():
+            position = r.json()['position']
+        else:
+            print(r.json())
+        return position
     def set_position(self, shade_mac, position):
-        r = requests.request('POST', self.host, json = { 'shade': shade_mac, 'action': 'target', 'value': position}, auth = (self.user, self.password))
+        while(1):
+            try:
+                r = requests.request('POST', self.host, json = { 'shade': shade_mac, 'action': 'target', 'value': position}, auth = (self.user, self.password))
+                break;
+            except requests.exceptions.ConnectionError:
+                pass
 
-test = ShadeClient(shade_config)
-battery = test.get_battery('F4:21:20:D9:FA:CD')
-position = test.get_position('F4:21:20:D9:FA:CD')
-print(battery, position)
-test.set_position('F4:21:20:D9:FA:CD', 50)
+#test = ShadeClient(shade_config)
+#battery = test.get_battery('F4:21:20:D9:FA:CD')
+#position = test.get_position('F4:21:20:D9:FA:CD')
+#print(battery, position)
+#test.set_position('F4:21:20:D9:FA:CD', 50)
