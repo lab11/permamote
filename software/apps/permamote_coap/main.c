f3eebb03c4741d869314fbce6743ceb69a2980df
Neal Jackson <nealjack@umich.edu>
2018-07-19 15:58:07 -0700

    coap app read id from flash

--- software/apps/permamote_coap/main.c
+++ software/apps/permamote_coap/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "nrf.h"
 #include "nrf_gpio.h"
 #include "nrf_delay.h"
 #include "nrf_twi_mngr.h"
@@ -19,6 +20,7 @@
 
 #include "simple_thread.h"
 #include "thread_coap.h"
+#include "device_id.h"
 
 #include "permamote.h"
 #include "max44009.h"
@@ -27,12 +29,13 @@
 #define LED1 NRF_GPIO_PIN_MAP(0,5)
 #define LED2 NRF_GPIO_PIN_MAP(0,6)
 
-#define COAP_SERVER_ADDR "64:ff9b::23a6:b3ac"
+#define COAP_SERVER_ADDR "64:ff9b::22da:2eb5"
 
 #define DEFAULT_CHILD_TIMEOUT    40                                         /**< Thread child timeout [s]. */
 #define DEFAULT_POLL_PERIOD      1000                                       /**< Thread Sleepy End Device polling period when MQTT-SN Asleep. [ms] */
 #define NUM_SLAAC_ADDRESSES      4                                          /**< Number of SLAAC addresses. */
 
+static uint8_t device_id[6];
 static otIp6Address m_peer_address =
 {
     .mFields =
@@ -45,19 +48,38 @@ static bool update_thresh = false;
 static float upper;
 static float lower;
 
-#define SENSOR_RATE APP_TIMER_TICKS(1000)
-APP_TIMER_DEF(sensor_read_timer);
+#define DISCOVER_PERIOD APP_TIMER_TICKS(5*60*1000)
+APP_TIMER_DEF(discover_send_timer);
 
 NRF_TWI_MNGR_DEF(twi_mngr_instance, 5, 0);
 
+static void discover_send_callback() {
+  char* addr = "j2x.us/perm";
+  uint8_t data[2 + 6 + strlen(addr)];
+  data[0] = 6;
+  memcpy(data+1, device_id, 6);
+  data[7] = strlen(addr);
+  memcpy(data+1+6+1, addr, strlen(addr));
+
+    thread_coap_send(thread_get_instance(), OT_COAP_CODE_PUT, OT_COAP_TYPE_NON_CONFIRMABLE, &m_peer_address, "discovery", data, 2 + 6 + strlen(addr));
+}
+
 static void sensor_read_callback(float lux) {
-    upper = lux + lux* 0.05;
-    lower = lux - lux* 0.05;
-    NRF_LOG_INFO("Sensed: lux: %u, upper: %u, lower: %u", (uint32_t)lux, (uint32_t)upper, (uint32_t)lower);
+  upper = lux + lux* 0.05;
+  lower = lux - lux* 0.05;
+  NRF_LOG_INFO("Sensed: lux: %u, upper: %u, lower: %u", (uint32_t)lux, (uint32_t)upper, (uint32_t)lower);
+  //NRF_LOG_INFO("ID[0]: %x", ((uint8_t *)(ID_FLASH_LOCATION))[0]);
+  //NRF_LOG_INFO("ADDR[0]: %lx", NRF_FICR->DEVICEADDR[0]);
+  //NRF_LOG_INFO("ADDR[1]: %lx", NRF_FICR->DEVICEADDR[1] & 0xFFFF);
 
-    update_thresh = true;
+  update_thresh = true;
 
-    thread_coap_send(thread_get_instance(), OT_COAP_CODE_PUT, OT_COAP_TYPE_NON_CONFIRMABLE, &m_peer_address, "light_lux", (uint8_t *)&lux, sizeof(float));
+  uint8_t data[1 + 6 + sizeof(float)];
+  data[0] = 6;
+  memcpy(data+1, device_id, 6);
+  memcpy(data+1+6, &lux, sizeof(float));
+
+  thread_coap_send(thread_get_instance(), OT_COAP_CODE_PUT, OT_COAP_TYPE_NON_CONFIRMABLE, &m_peer_address, "light_lux", data, 1+6+sizeof(float));
 }
 
 static void interrupt_callback(void) {
@@ -72,6 +94,16 @@ static void log_init(void)
     APP_ERROR_CHECK(err_code);
 
     NRF_LOG_DEFAULT_BACKENDS_INIT();
+}
+
+static void timer_init(void)
+{
+  uint32_t err_code = app_timer_init();
+  APP_ERROR_CHECK(err_code);
+  err_code = app_timer_create(&discover_send_timer, APP_TIMER_MODE_REPEATED, discover_send_callback);
+  APP_ERROR_CHECK(err_code);
+  err_code = app_timer_start(discover_send_timer, DISCOVER_PERIOD, NULL);
+  APP_ERROR_CHECK(err_code);
 }
 
 
@@ -97,12 +129,15 @@ int main(void) {
   // Init twi
   twi_init();
 
+  // init periodic timers
+  timer_init();
+
   // Init log
   log_init();
 
   NRF_LOG_INFO("\nLUX TEST\n");
 
-  //timer_init();
+  get_device_id(device_id);
 
   // Turn on power gate
   nrf_gpio_cfg_output(MAX44009_EN);
