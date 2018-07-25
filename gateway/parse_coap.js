39803a696418776afd2017e0b2095b0df1d99f58
Neal Jackson <nealjack@umich.edu>
2018-07-25 13:45:22 -0700

    add voltage parsing

--- gateway/parse_coap.js
+++ gateway/parse_coap.js
@@ -22,6 +22,11 @@ var parse_payload = function (device_id, resource_url, payload, cb) {
       else if (resource_url === '/humidity_percent') {
         out.humidity_percent = payload.readFloatLE();
       }
+      else if (resource_url === '/voltage') {
+        out.vbat = payload.readFloatLE();
+        out.vsol = payload.readFloatLE(4);
+        out.vsec = payload.readFloatLE(8);
+      }
       else {
         cb(null);
         return;
