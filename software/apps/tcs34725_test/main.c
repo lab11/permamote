b86cc19eaf8bc2a0a7372fe693edd60433dace1f
Neal Jackson <nealjack@umich.edu>
2018-08-28 13:25:34 -0700

    start of test app for tcs

---
+++ software/apps/tcs34725_test/main.c
@@ -0,0 +1,146 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "nrf_gpio.h"
+#include "nrf_delay.h"
+#include "nrf_twi_mngr.h"
+#include "app_util_platform.h"
+#include "nordic_common.h"
+#include "app_timer.h"
+#include "app_uart.h"
+#include "nrf_drv_clock.h"
+#include "nrf_power.h"
+
+#include "permamote.h"
+#include "tcs34725.h"
+
+#define UART_TX_BUF_SIZE     256
+#define UART_RX_BUF_SIZE     256
+
+#define LED0 NRF_GPIO_PIN_MAP(0,4)
+#define LED1 NRF_GPIO_PIN_MAP(0,5)
+#define LED2 NRF_GPIO_PIN_MAP(0,6)
+
+bool update_thresh = false;
+float upper;
+float lower;
+
+#define SENSOR_RATE APP_TIMER_TICKS(1000)
+APP_TIMER_DEF(sensor_read_timer);
+
+NRF_TWI_MNGR_DEF(twi_mngr_instance, 5, 0);
+
+void uart_error_handle (app_uart_evt_t * p_event) {
+    if (p_event->evt_type == APP_UART_COMMUNICATION_ERROR) {
+        APP_ERROR_HANDLER(p_event->data.error_communication);
+    } else if (p_event->evt_type == APP_UART_FIFO_ERROR) {
+        APP_ERROR_HANDLER(p_event->data.error_code);
+    }
+}
+
+static void sensor_read_callback(float lux) {
+    upper = lux + lux* 0.10;
+    lower = lux - lux* 0.10;
+    printf("\n#######\nlux: %f, upper: %f, lower: %f\n", lux, upper, lower);
+
+    update_thresh = true;
+}
+
+
+//static void timer_init(void)
+//{
+//    uint32_t err_code = app_timer_init();
+//    APP_ERROR_CHECK(err_code);
+//    err_code = app_timer_create(&sensor_read_timer, APP_TIMER_MODE_REPEATED, read_timer_callback);
+//    APP_ERROR_CHECK(err_code);
+//    err_code = app_timer_start(sensor_read_timer, SENSOR_RATE, NULL);
+//    APP_ERROR_CHECK(err_code);
+//}
+
+void uart_init(void) {
+  uint32_t err_code;
+
+  const app_uart_comm_params_t comm_params =
+  {
+    UART_RX,
+    UART_TX,
+    0,
+    0,
+    APP_UART_FLOW_CONTROL_DISABLED,
+    false,
+    UART_BAUDRATE_BAUDRATE_Baud115200
+  };
+  APP_UART_FIFO_INIT(&comm_params,
+                     UART_RX_BUF_SIZE,
+                     UART_TX_BUF_SIZE,
+                     uart_error_handle,
+                     APP_IRQ_PRIORITY_LOW,
+                     err_code);
+  APP_ERROR_CHECK(err_code);
+
+}
+
+void twi_init(void) {
+  ret_code_t err_code;
+
+  const nrf_drv_twi_config_t twi_config = {
+    .scl                = I2C_SCL,
+    .sda                = I2C_SDA,
+    .frequency          = NRF_TWI_FREQ_400K,
+  };
+
+  //err_code = nrf_drv_twi_init(&twi_instance, &twi_config, NULL, NULL);
+  //APP_ERROR_CHECK(err_code);
+
+  err_code = nrf_twi_mngr_init(&twi_mngr_instance, &twi_config);
+  APP_ERROR_CHECK(err_code);
+}
+
+int main(void) {
+  // init softdevice
+  //nrf_sdh_enable_request();
+  //sd_power_dcdc_mode_set(NRF_POWER_DCDC_ENABLE);
+  nrf_power_dcdcen_set(1);
+
+  // init led
+  //led_init(LED2);
+  //led_off(LED2);
+
+  // init uart
+  uart_init();
+
+  printf("\nLUX TEST\n");
+
+  // Init twi
+  twi_init();
+
+  //timer_init();
+
+  // Turn on power gate
+  nrf_gpio_cfg_output(MAX44009_EN);
+  nrf_gpio_cfg_output(TCS34725_EN);
+  nrf_gpio_cfg_output(MS5637_EN);
+  nrf_gpio_cfg_output(SI7021_EN);
+  nrf_gpio_pin_set(MAX44009_EN);
+  nrf_gpio_pin_set(TCS34725_EN);
+  nrf_gpio_pin_set(MS5637_EN);
+  nrf_gpio_pin_set(SI7021_EN);
+
+  tcs34725_config_t config = {
+    .int_time = TCS34725_INTEGRATIONTIME_154MS,
+    .gain = TCS34725_GAIN_1X,
+  };
+
+  tcs34725_init(&twi_mngr_instance);
+  tcs34725_config(config);
+  tcs34725_enable();
+
+  uint16_t red, green, blue, clear;
+
+  while (1) {
+    tcs34725_read_channels(&red, &green, &blue, &clear);
+    printf("raw channels: %d, %d, %d\n", red, green, blue);
+    nrf_delay_ms(5000);
+  }
+}
