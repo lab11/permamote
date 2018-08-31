0163576dd501fef43c393f5551f6a5c683e5284a
Neal Jackson <nealjack@umich.edu>
2018-08-30 20:15:55 -0700

    use underlying spi driver

--- software/chips/ab1815.c
+++ software/chips/ab1815.c
@@ -21,43 +21,66 @@ void ab1815_init(const nrf_spi_mngr_t* instance) {
   spi_config.bit_order  = NRF_DRV_SPI_BIT_ORDER_MSB_FIRST;
 }
 
+void  ab1815_read_reg(uint8_t reg, uint8_t* read_buf, size_t len){
+  if (len > 256) return;
+  uint8_t readreg = reg;
+  uint8_t buf[257];
+
+  nrf_drv_spi_uninit(&(spi_instance->spi));
+  nrf_drv_spi_init(&(spi_instance->spi), &spi_config, NULL, NULL);
+  nrf_drv_spi_transfer(&(spi_instance->spi), &readreg, 1, buf, len+1);
+
+  memcpy(read_buf, buf+1, len);
+}
+
+void ab1815_write_reg(uint8_t reg, uint8_t* write_buf, size_t len){
+  if (len > 256) return;
+  uint8_t buf[257];
+  buf[0] = 0x80 | reg;
+  memcpy(buf+1, write_buf, len);
+
+  nrf_drv_spi_uninit(&(spi_instance->spi));
+  nrf_drv_spi_init(&(spi_instance->spi), &spi_config, NULL, NULL);
+  nrf_drv_spi_transfer(&(spi_instance->spi), buf, len+1, NULL, 0);
+}
+
 void ab1815_set_config(ab1815_control_t config) {
-  uint8_t write[2];
-  write[0] = AB1815_CONTROL1 | 0x80;
-  write[1] =  config.stop << 7 | config.hour_12 << 6 | config.OUTB << 5 |
+  uint8_t write;
+  write =  config.stop << 7 | config.hour_12 << 6 | config.OUTB << 5 |
             config.OUT << 4 | config.rst_pol << 3 | config.auto_rst << 2 |
             0x2 | config.write_rtc;
   ctrl_config = config;
 
-  nrf_spi_mngr_transfer_t const config_transfer[] = {
-    NRF_SPI_MNGR_TRANSFER(write, 2, NULL, 0),
-  };
+  //nrf_spi_mngr_transfer_t const config_transfer[] = {
+  //  NRF_SPI_MNGR_TRANSFER(write, 2, NULL, 0),
+  //};
 
-  int error = nrf_spi_mngr_perform(spi_instance, &spi_config, config_transfer, 1, NULL);
-  APP_ERROR_CHECK(error);
+  //int error = nrf_spi_mngr_perform(spi_instance, &spi_config, config_transfer, 1, NULL);
+  //APP_ERROR_CHECK(error);
+  ab1815_write_reg(AB1815_CONTROL1, &write, 1);
 }
 
 void ab1815_get_config(ab1815_control_t* config) {
-  uint8_t write[1];
-  uint8_t read[2];
-  write[0] = AB1815_CONTROL1;
+  uint8_t read;
 
-  nrf_spi_mngr_transfer_t const config_transfer[] = {
-    NRF_SPI_MNGR_TRANSFER(write, 1, read, 2),
-  };
+  //nrf_spi_mngr_transfer_t const config_transfer[] = {
+  //  NRF_SPI_MNGR_TRANSFER(write, 1, read, 2),
+  //};
 
-  int error = nrf_spi_mngr_perform(spi_instance, &spi_config, config_transfer, 1, NULL);
-  APP_ERROR_CHECK(error);
+  //int error = nrf_spi_mngr_perform(spi_instance, &spi_config, config_transfer, 1, NULL);
+  //APP_ERROR_CHECK(error);
 
   //printf("%x\n", read[1]);
 
-  config->stop      = (read[1] & 0x80) >> 7;
-  config->hour_12   = (read[1] & 0x40) >> 6;
-  config->OUTB      = (read[1] & 0x20) >> 5;
-  config->OUT       = (read[1] & 0x10) >> 4;
-  config->rst_pol   = (read[1] & 0x08) >> 3;
-  config->auto_rst  = (read[1] & 0x04) >> 2;
-  config->write_rtc = read[1] & 0x01;
+  ab1815_read_reg(AB1815_CONTROL1, &read, 1);
+
+  config->stop      = (read & 0x80) >> 7;
+  config->hour_12   = (read & 0x40) >> 6;
+  config->OUTB      = (read & 0x20) >> 5;
+  config->OUT       = (read & 0x10) >> 4;
+  config->rst_pol   = (read & 0x08) >> 3;
+  config->auto_rst  = (read & 0x04) >> 2;
+  config->write_rtc = read & 0x01;
 }
 
 void ab1815_interrupt_config(ab1815_int_config_t config) {
@@ -73,11 +96,13 @@ inline uint8_t get_ones(uint8_t x) {
 }
 
 void ab1815_set_time(ab1815_time_t time) {
-  uint8_t write[9];
+  uint8_t write[8];
 
   // Ensure rtc write bit is enabled
-  ctrl_config.write_rtc = 1;
-  ab1815_set_config(ctrl_config);
+  if (ctrl_config.write_rtc != 1) {
+    ctrl_config.write_rtc = 1;
+    ab1815_set_config(ctrl_config);
+  }
 
   APP_ERROR_CHECK_BOOL(time.hundredths < 100 && time.hundredths >= 0);
   APP_ERROR_CHECK_BOOL(time.seconds < 60 && time.seconds>= 0);
@@ -88,44 +113,45 @@ void ab1815_set_time(ab1815_time_t time) {
   APP_ERROR_CHECK_BOOL(time.years < 100 && time.date >= 0);
   APP_ERROR_CHECK_BOOL(time.weekday < 7 && time.weekday >= 0);
 
-  write[0] = AB1815_HUND | 0x80;
-  write[1] = (get_tens(time.hundredths) & 0xF) << 4  | (get_ones(time.hundredths) & 0xF);
-  write[2] = (get_tens(time.seconds) & 0x7) << 4    | (get_ones(time.seconds) & 0xF);
-  write[3] = (get_tens(time.minutes) & 0x7) << 4    | (get_ones(time.minutes) & 0xF);
-  write[4] = (get_tens(time.hours) & 0x3) << 4      | (get_ones(time.hours) & 0xF);
-  write[5] = (get_tens(time.date) & 0x3) << 4       | (get_ones(time.date) & 0xF);
-  write[6] = (get_tens(time.months) & 0x1) << 4     | (get_ones(time.months) & 0xF);
-  write[7] = (get_tens(time.years) & 0xF) << 4      | (get_ones(time.years) & 0xF);
-  write[8] = time.weekday & 0x7;
+  write[0] = (get_tens(time.hundredths) & 0xF) << 4  | (get_ones(time.hundredths) & 0xF);
+  write[1] = (get_tens(time.seconds) & 0x7) << 4    | (get_ones(time.seconds) & 0xF);
+  write[2] = (get_tens(time.minutes) & 0x7) << 4    | (get_ones(time.minutes) & 0xF);
+  write[3] = (get_tens(time.hours) & 0x3) << 4      | (get_ones(time.hours) & 0xF);
+  write[4] = (get_tens(time.date) & 0x3) << 4       | (get_ones(time.date) & 0xF);
+  write[5] = (get_tens(time.months) & 0x1) << 4     | (get_ones(time.months) & 0xF);
+  write[6] = (get_tens(time.years) & 0xF) << 4      | (get_ones(time.years) & 0xF);
+  write[7] = time.weekday & 0x7;
 
-  nrf_spi_mngr_transfer_t const write_time_transfer[] = {
-    NRF_SPI_MNGR_TRANSFER(write, 9, NULL, 0),
-  };
+  ab1815_write_reg(AB1815_HUND, write, 8);
 
-  int error = nrf_spi_mngr_perform(spi_instance, &spi_config, write_time_transfer, 1, NULL);
-  APP_ERROR_CHECK(error);
+  //nrf_spi_mngr_transfer_t const write_time_transfer[] = {
+  //  NRF_SPI_MNGR_TRANSFER(write, 9, NULL, 0),
+  //};
+
+  //int error = nrf_spi_mngr_perform(spi_instance, &spi_config, write_time_transfer, 1, NULL);
+  //APP_ERROR_CHECK(error);
 }
 
 void ab1815_get_time(ab1815_time_t* time) {
-  uint8_t write[1];
   uint8_t read[10];
-  write[0] = AB1815_HUND;
 
-  nrf_spi_mngr_transfer_t const config_transfer[] = {
-    NRF_SPI_MNGR_TRANSFER(write, 1, read, 9),
-  };
+  ab1815_read_reg(AB1815_HUND, read, 8);
 
-  int error = nrf_spi_mngr_perform(spi_instance, &spi_config, config_transfer, 1, NULL);
+  //nrf_spi_mngr_transfer_t const config_transfer[] = {
+  //  NRF_SPI_MNGR_TRANSFER(write, 1, read, 9),
+  //};
 
-  time->hundredths = 10 * ((read[1] & 0xF0) >> 4) + (read[1] & 0xF);
-  time->seconds   = 10 * ((read[2] & 0x70) >> 4) + (read[2] & 0xF);
-  time->minutes   = 10 * ((read[3] & 0x70) >> 4) + (read[3] & 0xF);
+  //int error = nrf_spi_mngr_perform(spi_instance, &spi_config, config_transfer, 1, NULL);
+
+  time->hundredths = 10 * ((read[0] & 0xF0) >> 4) + (read[0] & 0xF);
+  time->seconds   = 10 * ((read[1] & 0x70) >> 4) + (read[1] & 0xF);
+  time->minutes   = 10 * ((read[2] & 0x70) >> 4) + (read[2] & 0xF);
   // TODO handle 12 hour format
-  time->hours     = 10 * ((read[4] & 0x30) >> 4) + (read[4] & 0xF);
-  time->date      = 10 * ((read[5] & 0x30) >> 4) + (read[5] & 0xF);
-  time->months    = 10 * ((read[6] & 0x10) >> 4) + (read[6] & 0xF);
-  time->years     = 10 * ((read[7] & 0xF0) >> 4) + (read[7] & 0xF);
-  time->weekday   = read[8] & 0x7;
+  time->hours     = 10 * ((read[3] & 0x30) >> 4) + (read[3] & 0xF);
+  time->date      = 10 * ((read[4] & 0x30) >> 4) + (read[4] & 0xF);
+  time->months    = 10 * ((read[5] & 0x10) >> 4) + (read[5] & 0xF);
+  time->years     = 10 * ((read[6] & 0xF0) >> 4) + (read[6] & 0xF);
+  time->weekday   = read[7] & 0x7;
 }
 
 ab1815_time_t unix_to_ab1815(struct timeval tv) {
