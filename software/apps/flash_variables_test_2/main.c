176653e02afde360e82f63707cfc7ea83a0f8d31
Henry Muller <henrymuller@berkeley.edu>
2019-03-07 15:45:38 -0800

    Perform first FDS write

--- software/apps/flash_variables_test_2/main.c
+++ software/apps/flash_variables_test_2/main.c
@@ -54,18 +54,58 @@ static void fds_evt_handler(fds_evt_t const * p_fds_evt)
 int main(void) {
     nrf_power_dcdcen_set(1);
     log_init();
+    NRF_LOG_INFO("Start");
 
-    ret_code_t ret = fds_register(fds_evt_handler);
-    if (ret != FDS_SUCCESS)
+    ret_code_t rc = fds_register(fds_evt_handler);
+    if (rc != FDS_SUCCESS)
     {
         // Registering of the FDS event handler has failed.
         NRF_LOG_INFO("FDS register failed");
+    } else {
+        NRF_LOG_INFO("FDS register OK");
     }
     
-    ret = fds_init();
-    if (ret != FDS_SUCCESS)
+    rc = fds_init();
+    if (rc != FDS_SUCCESS)
     {
         NRF_LOG_INFO("FDS init failed");
+    } else {
+        NRF_LOG_INFO("FDS init OK");
     }
 
+    uint16_t FILE_ID = 0x0001;  /* The ID of the file to write the records into. */
+    uint16_t RECORD_KEY_1 = 0x1111;  /* A key for the first record. */
+    uint16_t RECORD_KEY_2 = 0x2222;  /* A key for the second record. */
+    static uint32_t const m_deadbeef = 0xDEADBEEF;
+    static char const m_hello[]  = "Hello, world!";
+    fds_record_t        record;
+    fds_record_desc_t   record_desc;
+
+    // Set up record.
+    record.file_id           = FILE_ID;
+    record.key               = RECORD_KEY_1;
+    record.data.p_data       = &m_deadbeef;
+    record.data.length_words = 1;   /* one word is four bytes. */
+    
+    rc = fds_record_write(&record_desc, &record);
+    if (rc != FDS_SUCCESS)
+    {
+        NRF_LOG_INFO("FDS write 1 failed");
+    } else {
+        NRF_LOG_INFO("FDS write 1 OK");
+    }
+
+    // Set up record.
+    record.file_id           = FILE_ID;
+    record.key               = RECORD_KEY_2;
+    record.data.p_data       = &m_hello;
+    /* The following calculation takes into account any eventual remainder of the division. */
+    record.data.length_words = (sizeof(m_hello) + 3) / 4;
+    rc = fds_record_write(&record_desc, &record);
+    if (rc != FDS_SUCCESS)
+    {
+        NRF_LOG_INFO("FDS write 2 failed");
+    } else {
+        NRF_LOG_INFO("FDS write 2 OK");
+    }
 }
