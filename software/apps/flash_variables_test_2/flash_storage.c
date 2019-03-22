ebcafe82743f5e59394836f6464418f771b12eea
Henry Muller <henrymuller@berkeley.edu>
2019-03-22 13:27:14 -0700

    Pivot away from tag-struct strategy

--- software/apps/flash_variables_test_2/flash_storage.c
+++ software/apps/flash_variables_test_2/flash_storage.c
@@ -4,18 +4,11 @@
 #include "fds.h"
 #include "nrf_log.h"
 
-#define NAME_MAP_SIZE 20
-#define NAME_MAP_FILE_ID 0x0001
-#define NAME_MAP_RECORD_KEY 0x0001
 #define DEFAULT_FILE_ID 0x1111
+#define DEFAULT_RECORD_KEY 0x2222
 
-static const char *flash_variable_names[20] = {""};
-static int next_record_key = 1; // this might get reset every time the thing restarts, which we don't want
-
-typedef struct {
-    char *name;
-    void *p_value;
-} flash_storage_tag_t;
+#define FLASH_STORAGE_FLAG_FILE_ID 0x1010
+#define FLASH_STORAGE_FLAG_RECORD_KEY 0x9090
 
 static void fds_evt_handler(fds_evt_t const * p_fds_evt) {
     switch (p_fds_evt->id) {
@@ -39,9 +32,9 @@ static ret_code_t fds_write(uint16_t file_id, uint16_t record_key, void const *p
     return fds_record_write(&record_desc, &record);
 }
 
-static void name_map_init() {
-    const char *name_map[NAME_MAP_SIZE] = {""};
-    fds_write(NAME_MAP_FILE_ID, NAME_MAP_RECORD_KEY, &name_map, sizeof(name_map));
+static void set_flash_storage_flag() {
+    int x = 1; // actual value doesn't matter -- just matters that record exists
+    fds_write(FLASH_STORAGE_FLAG_FILE_ID, FLASH_STORAGE_FLAG_RECORD_KEY, &x, sizeof(x));
 }
 
 void flash_storage_init() {
@@ -61,9 +54,9 @@ void flash_storage_init() {
     fds_record_desc_t record_desc;
     fds_find_token_t ftok;
     memset(&ftok, 0x00, sizeof(fds_find_token_t)); // Zero the token
-    rc = fds_record_find(NAME_MAP_FILE_ID, NAME_MAP_RECORD_KEY, &record_desc, &ftok);
-    if (rc == FDS_ERR_NOT_FOUND) { // ie the name map has never been put into flash
-        name_map_init();
+    rc = fds_record_find(FLASH_STORAGE_FLAG_FILE_ID, FLASH_STORAGE_FLAG_RECORD_KEY, &record_desc, &ftok);
+    if (rc == FDS_ERR_NOT_FOUND) { // ie we've never used FDS before
+        set_flash_storage_flag();
     }
 } 
 
@@ -72,6 +65,7 @@ static ret_code_t fds_update(uint16_t file_id, uint16_t record_key, void const *
     fds_record_desc_t record_desc;
     fds_find_token_t ftok;
     memset(&ftok, 0x00, sizeof(fds_find_token_t)); // Zero the token
+    // TODO: change this back to a while loop thing because we aren't going to vary record IDs anymore
     fds_record_find(file_id, record_key, &record_desc, &ftok);
     // Now record_desc should contain correct description
     
@@ -92,7 +86,7 @@ static uint32_t fds_read(uint16_t file_id, uint16_t record_key) {
     fds_record_desc_t record_desc;
     fds_find_token_t ftok;
     memset(&ftok, 0x00, sizeof(fds_find_token_t)); // Zero the token
-
+    // TODO: change this back to a while loop thing because we aren't going to vary record IDs anymore
     if (fds_record_find(file_id, record_key, &record_desc, &ftok) == FDS_SUCCESS) {
         if (fds_record_open(&record_desc, &flash_record) != FDS_SUCCESS) {
             NRF_LOG_INFO("FDS read open failed");
@@ -107,49 +101,79 @@ static uint32_t fds_read(uint16_t file_id, uint16_t record_key) {
     return return_value;
 }
 
-static ret_code_t flash_write(uint16_t record_key, void const *p_data, size_t data_size) {
-    return fds_write(DEFAULT_FILE_ID, record_key, p_data, data_size);
-}
+// static uint16_t get_record_key(const char* name) {
+//     // Returns record_key if "name" is already present in array of flash variable names
+//     // Else, returns 0
+//     uint16_t record_key;
+//     fds_flash_record_t flash_record;
+//     fds_record_desc_t record_desc;
+//     fds_find_token_t ftok;
+//     memset(&ftok, 0x00, sizeof(fds_find_token_t)); // Zero the token
+//     fds_record_find(NAME_MAP_FILE_ID, NAME_MAP_RECORD_KEY, &record_desc, &ftok);
+//     fds_record_open(&record_desc, &flash_record);
 
-static uint16_t get_record_key(const char* name) {
-    // Returns record_key if "name" is already present in array of flash variable names
-    // Else, returns 0
-    uint16_t record_key;
-    fds_flash_record_t flash_record;
-    fds_record_desc_t record_desc;
-    fds_find_token_t ftok;
-    memset(&ftok, 0x00, sizeof(fds_find_token_t)); // Zero the token
-    fds_record_find(NAME_MAP_FILE_ID, NAME_MAP_RECORD_KEY, &record_desc, &ftok);
-    fds_record_open(&record_desc, &flash_record);
-
-    bool found = false;
-    for (record_key = 1; record_key < NAME_MAP_SIZE; record_key++) { // skip 0th entry because 0x0000 can't be used as a record ID
-        if (strcmp(name, (*((char**) flash_record.p_data))[record_key] == 0)) {
-            found = true;
-            break;
-        }
-    }
-    fds_record_close(&record_desc);
-    if (found) {
-        return record_key;
-    }
-    return 0;
-}
-
-static ret_code_t flash_update(uint16_t record_key, void const *p_data, size_t data_size) {
-    return fds_update(DEFAULT_FILE_ID, record_key, p_data, data_size);
-}
+//     bool found = false;
+//     for (record_key = 1; record_key < NAME_MAP_SIZE; record_key++) { // skip 0th entry because 0x0000 can't be used as a record ID
+//         if (strcmp(name, (*((char**) flash_record.p_data))[record_key] == 0)) {
+//             found = true;
+//             break;
+//         }
+//     }
+//     fds_record_close(&record_desc);
+//     if (found) {
+//         return record_key;
+//     }
+//     return 0;
+// }
 
 ret_code_t flash_put(const char *name, void const *p_data, size_t data_size) {
+    flash_storage_tag_t tag = {}
+
+    typedef struct {
+    char *name;
+    void *p_value;
+    } flash_storage_tag_t;
+ // make new struct with new data
+
     ret_code_t rc;
-    uint16_t record_key = get_record_key(name);
-    if (record_key > 0) { // Variable already exists
-        rc = flash_update(record_key, p_data, data_size);
-    } else {
-        rc = flash_write(next_record_key, p_data, data_size);
-        flash_variable_names[next_record_key] = name; // I don't think this works ahahaha
-        next_record_key++;
+    fds_flash_record_t  flash_record;
+    fds_record_desc_t   record_desc;
+    fds_find_token_t    ftok;
+    memset(&ftok, 0x00, sizeof(fds_find_token_t));
+
+    bool found = false;
+    while (fds_record_find(DEFAULT_FILE_ID, DEFAULT_RECORD_KEY, &record_desc, &ftok) == FDS_SUCCESS && !found) {
+        if (fds_record_open(&record_desc, &flash_record) == FDS_SUCCESS) {
+            if (strcmp(name, (flash_record.p_data)->name) == 0) {
+                found = true;
+            }
+        } else {
+            NRF_LOG_INFO("FDS read open failed");
+        }
+        if (fds_record_close(&record_desc) != FDS_SUCCESS) {
+             NRF_LOG_INFO("FDS read close failed");
+        }
     }
+
+    if (found) { // I.E. this is not a fresh variable and we're just updating
+        // stick struct into flash using update()
+        // This preserves an invariant that there's only ever one record at a time in flash with our "name"
+    } else {
+        // stick struct into flash using write()
+    }
+
+
+
+    } 
+
+    //uint16_t record_key = get_record_key(name);
+    //if (record_key > 0) { // Variable already exists
+    //    rc = fds_update(DEFAULT_FILE_ID, record_key, p_data, data_size);
+    //} else {
+    //    rc = fds_write(DEFAULT_FILE_ID, record_key, p_data, data_size);
+    //    flash_variable_names[next_record_key] = name; // I don't think this works ahahaha
+    //    next_record_key++;
+    //}
     return rc;
 }
 
