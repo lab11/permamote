c84541ed6be3c6b40bd343465c6552ef27caebd0
Henry Muller <henrymuller@berkeley.edu>
2019-04-02 20:45:36 -0700

    continue work

--- software/apps/flash_variables_test_2/flash_storage.c
+++ software/apps/flash_variables_test_2/flash_storage.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdint.h>
 #include <stddef.h>
 #include "nrf.h"
@@ -126,8 +127,16 @@ static uint32_t fds_read(uint16_t file_id, uint16_t record_key) {
 //     return 0;
 // }
 
+char *make_int_string(const char *name, int data) {
+    int length = snprintf(NULL, 0, "%x", data);
+    char* int_str = malloc( length + 1 );
+    snprintf(int_str, length + 1, "%x", data);
+    printf("%s\n", int_str);
+    free(int_str); 
+}
+
 ret_code_t flash_put(const char *name, void const *p_data, size_t data_size) {
-    
+
 
     ret_code_t rc;
     fds_flash_record_t  flash_record;
@@ -138,7 +147,7 @@ ret_code_t flash_put(const char *name, void const *p_data, size_t data_size) {
     bool found = false;
     while (fds_record_find(DEFAULT_FILE_ID, DEFAULT_RECORD_KEY, &record_desc, &ftok) == FDS_SUCCESS && !found) {
         if (fds_record_open(&record_desc, &flash_record) == FDS_SUCCESS) {
-            if (strcmp(name, (flash_record.p_data)->name) == 0) {
+            if (true) { // TODO: fix this
                 found = true;
             }
         } else {
@@ -150,16 +159,14 @@ ret_code_t flash_put(const char *name, void const *p_data, size_t data_size) {
     }
 
     if (found) { // I.E. this is not a fresh variable and we're just updating
+        return rc;
         // stick struct into flash using update()
         // This preserves an invariant that there's only ever one record at a time in flash with our "name"
     } else {
         // stick struct into flash using write()
+        return rc;
     }
-
-
-
-    } 
-
+ 
     //uint16_t record_key = get_record_key(name);
     //if (record_key > 0) { // Variable already exists
     //    rc = fds_update(DEFAULT_FILE_ID, record_key, p_data, data_size);
