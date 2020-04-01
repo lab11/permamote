41216c4b5b12ed2f5950842a016fd2ec620bbc11
Neal Jackson <nealjack@umich.edu>
2020-04-01 14:21:53 -0700

    add travis

---
+++ .travis/build_dir.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+for directory in `find $TRAVIS_BUILD_DIR -maxdepth 1 -mindepth 1 -type d -not -name .svn`
+do
+  echo $directory
+  cd $directory
+  if make -j
+  then
+    echo Success!
+  else
+    cd -
+    exit 1
+    fi
+    cd -
+done
